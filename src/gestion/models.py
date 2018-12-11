21fdddddd785395aa2a26d03768ec80b0e8ea6e9
cristian-casih <cristiancasih1@gmail.com>
2018-12-11 19:16:07 -0300

    segundo commit

--- src/gestion/models.py
+++ src/gestion/models.py
@@ -4,11 +4,23 @@ from django.db import models
 
 # Create your models here.
 class Personal (models.Model):
-    #dni = models.BigAutoField(max_length= 100)
-    nombre = models.CharField(max_length=100)
-    apellido = models.CharField(max_length=100)
+
+    dni = models.IntegerField(default=0,null= False,blank= False)
+    nombre = models.CharField(max_length=100,null= False,blank= False)
+    apellido = models.CharField(max_length=100,null= False,blank= False)
     email = models.EmailField()
 
     def __str__(self):
         return self.apellido
 
+class Inventario (models.Model):
+
+    id = models.IntegerField(default=0)
+    nombre = models.CharField(max_length=100,null= False,blank= False)
+    stockinicial = models.IntegerField(default=0)
+    stockactual = models.IntegerField(default=0)
+    estado = models.BooleanField()
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
