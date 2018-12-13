7c8e1499d10013bced37355295546dc64d5fdb1d
cristian-casih <cristiancasih1@gmail.com>
2018-12-13 12:46:10 -0300

    jueves 12:00

--- src/gestion/forms.py
+++ src/gestion/forms.py
@@ -1,5 +1,24 @@
 from django import forms
 
+from .models import Personal
+
+
+class RegModelForm(forms.ModelForm):
+    class Meta:
+        model = Personal
+        fields =["dni","nombre","apellido","email"]
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        email_base, proveeder = email.split("@")
+        dominio, extension = proveeder.split(".")
+        if not dominio == "intel":
+            raise forms.ValidationError("Por favor utiliza un mail con dominio @intel")
+        return email
+
+
 class RegForm (forms.Form):
+    dni = forms.IntegerField()
     nombre = forms.CharField(max_length=100)
-    edad = forms.IntegerField()
+    apellido = forms.CharField(max_length=100)
+    email = forms.EmailField()
--- src/gestion/views.py
+++ src/gestion/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 
 from .forms import RegForm
+from .models import Personal
 
 # Create your views here.
 
 def inicio(request):
-    form = RegForm()
+    form = RegForm(request.POST or None)
+    if form.is_valid():
+        form_data = form.cleaned_data
+        abc = form_data.get('dni')
+        abc1 = form_data.get('nombre')
+        abc2 = form_data.get('apellido')
+        abc3 = form_data.get('email')
+        obj = Personal.objects.create(dni=abc, nombre=abc1, apellido=abc2, email=abc3 )
     context = {
         "el_form": form,
     }
