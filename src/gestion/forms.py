d588ac828623fc9904c56e42df1f59ef950675df
cristian-casih <cristiancasih1@gmail.com>
2018-12-19 12:55:15 -0300

    miercoles 12.45

--- src/gestion/forms.py
+++ src/gestion/forms.py
@@ -12,13 +12,7 @@ class RegModelForm(forms.ModelForm):
         email = self.cleaned_data.get("email")
         email_base, proveeder = email.split("@")
         dominio, extension = proveeder.split(".")
+        print(dominio)
         if not dominio == "intel":
             raise forms.ValidationError("Por favor utiliza un mail con dominio @intel")
         return email
-
-
-class RegForm (forms.Form):
-    dni = forms.IntegerField()
-    nombre = forms.CharField(max_length=100)
-    apellido = forms.CharField(max_length=100)
-    email = forms.EmailField()
--- src/gestion/views.py
+++ src/gestion/views.py
@@ -1,21 +1,29 @@
 from django.shortcuts import render
 
 
-from .forms import RegForm
+from .forms import RegModelForm
 from .models import Personal
 
 # Create your views here.
 
 def inicio(request):
-    form = RegForm(request.POST or None)
-    if form.is_valid():
-        form_data = form.cleaned_data
-        abc = form_data.get('dni')
-        abc1 = form_data.get('nombre')
-        abc2 = form_data.get('apellido')
-        abc3 = form_data.get('email')
-        obj = Personal.objects.create(dni=abc, nombre=abc1, apellido=abc2, email=abc3)
+    titulo = "Hola"
+    if request.user.is_authenticated:
+        titulo = "Bienvenido %s" %(request.user)
+
+    form = RegModelForm(request.POST or None)
+
     context = {
+        "titulo": titulo,
         "el_form": form,
     }
-    return render(request, "inicio.html", context)
+    if form.is_valid():
+        apellido = form.cleaned_data.get("apellido")
+        form.save()
+        context = {
+            "titulo": "Registrado con exito %s!" %(apellido)
+        }
+
+
+
+    return render(request, "base.html", context)
