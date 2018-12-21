e32a87a13f4dde22454ab442b5aa492db9f23045
cristian-casih <cristiancasih1@gmail.com>
2018-12-21 12:16:28 -0300

    viernes

--- src/DjangoProject/settings.py
+++ src/DjangoProject/settings.py
@@ -127,4 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_env','static_root')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
