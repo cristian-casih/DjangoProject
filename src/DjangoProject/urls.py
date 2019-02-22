0d71f46bf4ff0b4dbf1f59fa14bb1e15b5304249
cristian-casih <cristiancasih1@gmail.com>
2019-02-22 12:46:19 -0300

    algunos cambios, no envia el mail

--- src/DjangoProject/urls.py
+++ src/DjangoProject/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.views.generic import RedirectView
+from django.contrib.auth import views
 
 
 
@@ -33,11 +34,10 @@ urlpatterns = [
     path('usuario/', include('usuario.urls', 'usuario')),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/',  auth_views.LogoutView.as_view(),name='logout'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('password_reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
-         name='password_reset_confirm'),
-    path('password_reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 ]
 if settings.DEBUG:
