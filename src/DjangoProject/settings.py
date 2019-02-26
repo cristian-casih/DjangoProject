c0acbcad030f3abf0f9fcb9d9063a56204efb3ed
cristian-casih <cristiancasih1@gmail.com>
2019-02-26 11:25:51 -0300

    se resolvio, funciona perfecto listo para el merge

--- src/DjangoProject/settings.py
+++ src/DjangoProject/settings.py
@@ -144,17 +144,9 @@ STATICFILES_FINDERS = [
 LOGIN_REDIRECT_URL = reverse_lazy('gestion:personal_list')
 LOGOUT_REDIRECT_URL = reverse_lazy('login')
 
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.Emailbackend'
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'cristiancasih1@gmail.com'
 EMAIL_HOST_PASSWORD = 'sqlserver#include12'
 
-
-
-
-
-#EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
