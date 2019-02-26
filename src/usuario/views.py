9074b5e7b1ba969f8dd6686c85267e28fd19d053
cristian-casih <cristiancasih1@gmail.com>
2019-02-26 12:45:47 -0300

    se direccionaron todos los templates para /login

--- src/usuario/views.py
+++ src/usuario/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from usuario.forms import RegistroForm
 
@@ -14,9 +15,14 @@ def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 
-class RegistroUsuario(CreateView):
+class RegistroUsuario(LoginRequiredMixin,CreateView):
+    login_url = '/accounts/login/'
+    redirect_field_name = 'redirect_to'
     model = User
     template_name = "usuario/registrar.html"
     form_class = RegistroForm
     success_url = reverse_lazy("gestion:personal_list")
 
+class InventarioCreate(LoginRequiredMixin,CreateView):
+    login_url = '/accounts/login/'
+    redirect_field_name = 'redirect_to'
