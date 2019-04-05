aab105c05a1ac18f7afdb8cd9b2d7b49288b8e5e
cristian-casih <cristiancasih1@gmail.com>
2019-04-05 12:54:52 -0300

    nuevos modelos

--- src/gestion/models.py
+++ src/gestion/models.py
@@ -1,7 +1,11 @@
 from __future__ import unicode_literals
-from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.fields import GenericForeignKey,GenericRelation
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.utils import timezone
+#from activity.models import Activity
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Personal(models.Model):
@@ -12,6 +16,9 @@ class Personal(models.Model):
     fecha_registro = models.DateTimeField(auto_now_add=True,null=True, blank=True)
     email = models.EmailField()
     estadoactivo = models.BooleanField(default=True,null=True, blank=True, editable=True)
+    pub_date = models.DateTimeField(default=timezone.now)
+    #activity = GenericRelation(Activity, related_query_name="personal")
+    activity = GenericRelation('Activity')
 
     def __str__(self):
         return "{}".format(self.apellido)
@@ -24,58 +31,54 @@ class Inventario(models.Model):
     estadoactivo = models.BooleanField(default=True,null=True, blank=True, editable=True)
     imagen = models.ImageField(blank=True, null=True, upload_to="inventario")
     descripcion = models.TextField()
+    pub_date = models.DateTimeField(default=timezone.now)
+    activity = GenericRelation('Activity')
 
 
-#
+class Activity(models.Model):
+    actor = models.ForeignKey(User, on_delete=models.CASCADE,)
+    #verb = models.PositiveIntegerField(choices=VERB_TYPE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    pub_date = models.DateTimeField(default=timezone.now)
+
+    def get_activity():
+        content_type = ContentType.objects.get_for_model(Inventario)
+        activities = Activity.objects.filter(
+            content_type=content_type,
+            object_id=Inventario.id,
+            date_to__gte=models.DateField.today()
+        )
+
+        return activities
+
 # class Activity(models.Model):
+#     ASIGNADO= 'A'
+#     ACT='B'
+#     ACTIVITY_TYPES = (
+#         (ASIGNADO, 'Asignado'),
+#         (ACT,'Actividad')
+#     )
+#
+#     inventario = ContentType.objects.get_for_model(Inventario)
+#     activity_type = models.CharField(max_length=1, choices=ACTIVITY_TYPES)
+#     date = models.DateTimeField(auto_now_add=True)
 #
 #     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
 #     object_id = models.PositiveIntegerField()
 #     content_object = GenericForeignKey('content_type', 'object_id')
 #
-#
-# def get_activities():
-#
-#     content_type = ContentType.objects.get_for_model(Inventario)
-#     inventario = Inventario.objects.filter(
-#         content_type=content_type,
-#         object_id=Inventario.id,
-#         date_to__gte=models.DateField.today()
+#     def get_activity():
+#         inventario = Inventario.objects.filter(
+#             content_type=content_type,
+#             object_id=Inventario.id,
+#             date_to__gte=models.DateField.today()
 #     )
 #
-#     return inventario
+#         return inventario
+#
 
-
-class Activity(models.Model):
-    ASIGNADO= 'A'
-    ACT='B'
-    ACTIVITY_TYPES = (
-        (ASIGNADO, 'Asignado'),
-        (ACT,'Actividad')
-    )
-
-    inventario = ContentType.objects.get_for_model(Inventario)
-    activity_type = models.CharField(max_length=1, choices=ACTIVITY_TYPES)
-    date = models.DateTimeField(auto_now_add=True)
-
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey('content_type', 'object_id')
-
-    def get_activity():
-        inventario = Inventario.objects.filter(
-            content_type=content_type,
-            object_id=Inventario.id,
-            date_to__gte=models.DateField.today()
-    )
-
-        return inventario
-
-
-
-#[11:13, 15/3/2019] Pablo Vates: agregues 2 metodos. uno que reciba el objeto y le agregue al objeto el contenttype y el contentid
-#[11:14, 15/3/2019] Pablo Vates: y en el objeto que estas agregando las actividad, por ejemplo en el inventario agrega un metodo que
-#sea algo asi como get_activity que filtre las actividade sy devuelva la lista de actividades relacionadas al objeto
 # class Activity(models.Model):
 #     FAVORITE = 'F'
 #     LIKE = 'L'
